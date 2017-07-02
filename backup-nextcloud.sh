7df8f0d42b0c749a7a595756588cb927474e9f8a
fischerman <code@bjorn-fischer.de>
2017-07-02 14:34:35 +0200

    change verbosity

--- backup-nextcloud.sh
+++ backup-nextcloud.sh
@@ -4,7 +4,7 @@ set -e
 
 kubectl exec $NEXTCLOUD_POD -- sudo -u www-data /var/www/html/occ maintenance:mode --on
 pg_dump $PGDATABASE > /backup-staging/db.sql
-rsync -av --delete /app-data /backup-staging
+rsync -a --delete /app-data /backup-staging
 kubectl exec $NEXTCLOUD_POD -- sudo -u www-data /var/www/html/occ maintenance:mode --off
 # rdiff-backup remote
 
