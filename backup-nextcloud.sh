04606f068ab5e3f336003ff73f8e6ed5845e9821
fischerman <code@bjorn-fischer.de>
2017-07-08 14:31:11 +0200

    Implements remote backup

--- backup-nextcloud.sh
+++ backup-nextcloud.sh
@@ -3,10 +3,21 @@
 set -e
 
 kubectl exec $NEXTCLOUD_POD -- sudo -u www-data /var/www/html/occ maintenance:mode --on
+
+START=$(date +%s.%N)
 pg_dump $PGDATABASE > /backup-staging/db.sql
 rsync -a --delete /app-data /backup-staging
+END=$(date +%s.%N)
+DIFF=$(echo "$END - $START" | bc)
+echo Took $DIFF seconds to copy to staging area
+
 kubectl exec $NEXTCLOUD_POD -- sudo -u www-data /var/www/html/occ maintenance:mode --off
-# rdiff-backup remote
+
+START=$(date +%s.%N)
+rdiff-backup --remote-schema 'ssh -i /ssh/ssh-privatekey -oStrictHostKeyChecking=no -C %s rdiff-backup --server' --print-statistics /backup-staging $DEST
+END=$(date +%s.%N)
+DIFF=$(echo "$END - $START" | bc)
+echo Took $DIFF seconds to backup to remote
 
 # PGDATABASE
 # PGHOST
