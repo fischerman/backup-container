03bbd09bf50c1f3f7a29c5195a23c888b8d030c0
fischerman <code@bjorn-fischer.de>
2018-08-28 16:47:05 +0200

    borg initial

--- backup-nextcloud.sh
+++ backup-nextcloud.sh
@@ -14,7 +14,9 @@ echo Took $DIFF seconds to copy to staging area
 kubectl exec $NEXTCLOUD_POD -- bash -c "echo '/var/www/html/occ maintenance:mode --off' > /tmp/moff.sh && chmod +x /tmp/moff.sh && runuser -u www-data /tmp/moff.sh"
 
 START=$(date +%s.%N)
-rdiff-backup --remote-schema 'ssh -i /ssh/ssh-privatekey -oStrictHostKeyChecking=no -C %s rdiff-backup --server' --print-statistics /backup-staging $DEST
+export BORG_UNKNOWN_UNENCRYPTED_REPO_ACCESS_IS_OK=yes
+export BORG_RSH="ssh -i /ssh/ssh-privatekey -oStrictHostKeyChecking=no -oBatchMode=yes" 
+borg create --verbose --stats --show-rc --exclude-caches $DEST::'nextcloud-{now}' /backup-staging
 END=$(date +%s.%N)
 DIFF=$(echo "$END - $START" | bc)
 echo Took $DIFF seconds to backup to remote
