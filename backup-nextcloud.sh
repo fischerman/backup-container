3a8d039fddbef5c26f86949e922c31f1ea996e7b
fischerman <code@bjorn-fischer.de>
2022-07-12 22:50:48 +0200

    most recent changes from spectre

--- backup-nextcloud.sh
+++ backup-nextcloud.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-set -e
+set -ex
+
+if [ -d /backup-staging/backup ]; then
+    # set to true so that it can be deleted
+    btrfs property set -ts /backup-staging/backup ro true
+    btrfs subvolume delete -c /backup-staging/backup
+fi
 
 kubectl exec $NEXTCLOUD_POD -- bash -c "echo '/var/www/html/occ maintenance:mode --on' > /tmp/mon.sh && chmod +x /tmp/mon.sh && runuser -u www-data /tmp/mon.sh"
 
 START=$(date +%s.%N)
-pg_dump $PGDATABASE > /backup-staging/db.sql
-rsync -a --delete /app-data /backup-staging
+btrfs subvolume snapshot /app-data /backup-staging/backup
+pg_dump $PGDATABASE > /backup-staging/backup/db.sql
+btrfs property set -ts /backup-staging/backup ro true
 END=$(date +%s.%N)
 DIFF=$(echo "$END - $START" | bc)
 echo Took $DIFF seconds to copy to staging area
@@ -16,7 +23,7 @@ kubectl exec $NEXTCLOUD_POD -- bash -c "echo '/var/www/html/occ maintenance:mode
 START=$(date +%s.%N)
 export BORG_UNKNOWN_UNENCRYPTED_REPO_ACCESS_IS_OK=yes
 export BORG_RSH="ssh -i /ssh/ssh-privatekey -oStrictHostKeyChecking=no -oBatchMode=yes" 
-borg create --verbose --stats --show-rc --exclude-caches $DEST::'nextcloud-{now}' /backup-staging
+borg create --verbose --stats --show-rc --exclude-caches $BORG_OPTION $DEST::'nextcloud-{now}' /backup-staging/backup
 END=$(date +%s.%N)
 DIFF=$(echo "$END - $START" | bc)
 echo Took $DIFF seconds to backup to remote
